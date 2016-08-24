5192133d34d5c7f484d486f098f8971d3abf5eb0
azharmalik3 <azharmalik02@gmail.com>
2016-08-24 12:44:07 +0500

    Added check if new image same as last captured image then don't save

--- lib/evercam_media/snapshot/storage_handler.ex
+++ lib/evercam_media/snapshot/storage_handler.ex
@@ -9,7 +9,13 @@ defmodule EvercamMedia.Snapshot.StorageHandler do
 
   def handle_event({:got_snapshot, data}, state) do
     {camera_exid, timestamp, image} = data
-    spawn fn -> Storage.save(camera_exid, timestamp, image, "Evercam Proxy") end
+    spawn fn ->
+      last_image = ConCache.get(:last_snapshot, camera_exid)
+      if last_image != image do
+        Storage.save(camera_exid, timestamp, image, "Evercam Proxy")
+      end
+      ConCache.put(:last_snapshot, camera_exid, image)
+    end
     {:ok, state}
   end
 
